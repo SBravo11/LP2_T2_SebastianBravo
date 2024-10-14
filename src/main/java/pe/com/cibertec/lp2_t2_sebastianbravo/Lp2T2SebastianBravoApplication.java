6a5712503eadbeae4a8aff8aec8011575fc4abfd
Sebastian Bravo <c_sbravo@outlook.com>
2024-10-13 20:26:56 -0500

    Initial commit

---
+++ src/main/java/pe/com/cibertec/lp2_t2_sebastianbravo/Lp2T2SebastianBravoApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.lp2_t2_sebastianbravo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2SebastianBravoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lp2T2SebastianBravoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/com/cibertec/lp2_t2_sebastianbravo/Lp2T2SebastianBravoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.lp2_t2_sebastianbravo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2SebastianBravoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
